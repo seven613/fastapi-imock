80f6db0fcc292b3934af0b96b3863fcee7f01213
seven613 <seven613@126.com>
2021-07-02 17:27:10 +0800

    函数依赖

--- tutorial/chapter05.py
+++ tutorial/chapter05.py
@@ -8,7 +8,43 @@
 @版本        :1.0
 '''
 
-
-from fastapi import APIRouter
+from typing import Optional
+from fastapi import APIRouter, Depends
 
 app05 = APIRouter()
+
+
+""" 创建、导入和声明依赖"""
+
+# 定义依赖函数
+
+
+async def common_parameters(q: Optional[str] = None, page: int = 1, limit: int = 10):
+    return{'q': q, 'page': page, "limit": limit}
+
+
+@app05.get('/dependency01')
+async def dependency01(commons: dict = Depends(common_parameters)):  # 异步函数使用依赖
+    return commons
+
+
+@app05.get('/dependency02')
+def dependency02(commons: dict = Depends(common_parameters)):  # 非异步函数使用依赖
+    return commons
+
+
+""" 将类作为依赖项"""
+
+fake_items_db = [{'item_name': "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: Optional[str] = None, page: int = 1, limit: int = 10):
+        self.q = q
+        self.page = page
+        self.limit = limit
+@app05.get("/classes_as_dependencies")
+async def class_as_dependencies(commons:CommonQueryParams=Depends(CommonQueryParams)):
+async def class_as_dependencies(commons:CommonQueryParams=Depends(CommonQueryParams)):
+async def class_as_dependencies(commons:CommonQueryParams=Depends(CommonQueryParams)):
