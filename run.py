46cc702b8dca14be7547430398343c27f21cffb3
seven613 <seven613@126.com>
2021-07-02 15:52:33 +0800

    静态文件引入，重写异常处理

--- run.py
+++ run.py
@@ -7,16 +7,44 @@
 @作者        :张强
 @版本        :1.0
 '''
+from starlette.applications import Starlette
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-from tutorial import app03
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+from fastapi.exceptions import HTTPException
 
-app = FastAPI()
 
-#生效子应用
-app.include_router(app03,prefix='/chapter03',tags=['第三章 请求参数与验证']) #include_router 路由生效，第一个参数：子应用，第二个参数：路径前缀，第三个参数：文档标签
+from tutorial import app03, app04, app05
 
+
+app = FastAPI(
+    title='项目标题',
+    description="项目的描述",
+    version='1.0.1'
+)
+# mount表示将某个目录下一个完全独立的应用挂载过来，这个不会在API交互文档中显示
+app.mount(path='/static',
+          app=StaticFiles(directory='./coronavrius/static'), name='static')
+
+
+@app.exception_handler(HTTPException)  # 重写HTTPException异常处理器
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)  # 重写请求验证异常处理器
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
+
+
+# 生效子应用
+# include_router 路由生效，第一个参数：子应用，第二个参数：路径前缀，第三个参数：文档标签
+app.include_router(app03, prefix='/chapter03', tags=['第三章 请求参数与验证'])
+app.include_router(app04, prefix='/chapter04', tags=['第四章 响应处理和FastAPI配置'])
+app.include_router(app05, prefix='/chapter05', tags=['第五章 FastAPI的依赖注入系统'])
 
 if __name__ == '__main__':
     uvicorn.run('run:app',
--- tutorial/chapter05.py
+++ tutorial/chapter05.py
@@ -1,2 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@文件        :chapter05.py
+@说明        :
+@时间        :2021/07/02 14:27:40
+@作者        :张强
+@版本        :1.0
+'''
 
 
+from fastapi import APIRouter
+
+app05 = APIRouter()
